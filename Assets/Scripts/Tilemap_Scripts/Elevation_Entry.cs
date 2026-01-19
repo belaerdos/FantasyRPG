1386108ab6ef6bd0c9d9f8e36010c842a3724711
belaerdos <140401957+belaerdos@users.noreply.github.com>
2026-01-19 07:38:27 +0100

    Started elevation logic

---
+++ Assets/Scripts/Tilemap_Scripts/Elevation_Entry.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Elevation_Entry : MonoBehaviour
+{
+    public Collider2D[] mountainColliders;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            foreach (Collider2D mountain in mountainColliders)
+            {
+                mountain.enabled = false;
+            }
+            collision.gameObject.GetComponent<SpriteRenderer>().sortingOrder = 15;
+        }
+    }
+}

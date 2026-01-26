4618a5de950bf38fba2ea25b87dfabbad6690232
belaerdos <140401957+belaerdos@users.noreply.github.com>
2026-01-26 07:37:20 +0100

    fixed enemy bug and created enemy anims

--- Assets/Scripts/Enemy_Movement.cs
+++ Assets/Scripts/Enemy_Movement.cs
@@ -2,40 +2,49 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEditor.Tilemaps;
 using UnityEngine;
+using UnityEngine.XR;
 
 public class Enemy_Movement : MonoBehaviour
 {
     public float speed;
-    private bool isChasing;
     private int facingDirection = 1;
+    private EnemyState enemyState;
 
     private Rigidbody2D rb;
     private Transform player;
+    private Animator anim;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        ChangeState(EnemyState.Idle);
     }
 
     void Update()
     {
-        if (isChasing)
-        {
-            if (player.position.x > transform.position.x && facingDirection == 1 ||
-                player.position.x < transform.position.x && facingDirection == -1)
-            {
-                Flip();
-            }
-            Vector2 direction = (player.position - transform.position).normalized;
-            rb.velocity = direction * speed;
-        }
-        
+        if ((enemyState == EnemyState.Idle) || player == null)
+            return;
+
+        // Flip toward player
+        if (player.position.x > transform.position.x && facingDirection == -1)
+            Flip();
+        else if (player.position.x < transform.position.x && facingDirection == 1)
+            Flip();
+
+        // Move
+        Vector2 direction = (player.position - transform.position).normalized;
+        rb.velocity = direction * speed;
+
     }
 
     void Flip()
     {
         facingDirection *= -1;
-        transform.localScale = new Vector3(transform.localScale.x * -1, transform.localScale.y, transform.localScale.z);
+        transform.localScale = new Vector3(
+            transform.localScale.x * -1,
+            transform.localScale.y,
+            transform.localScale.z);
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -43,8 +52,10 @@ public class Enemy_Movement : MonoBehaviour
         if (collision.gameObject.tag == "Player")
         {
             if (player == null)
+            {
                 player = collision.transform;
-            isChasing = true;
+            } 
+            ChangeState(EnemyState.Chasing);
         }
             
     }
@@ -54,9 +65,32 @@ public class Enemy_Movement : MonoBehaviour
         if (collision.gameObject.tag == "Player")
         {
             rb.velocity = Vector2.zero;
-            isChasing = false;
+            ChangeState(EnemyState.Idle);
         }
             
     }
 
+    void ChangeState(EnemyState newState)
+    {
+        //Exit current animation
+        if (enemyState == EnemyState.Idle)
+            anim.SetBool("isIdle", false);
+        else if (enemyState == EnemyState.Chasing)
+            anim.SetBool("isChasing", false);
+
+        //Set our current state
+        enemyState = newState;
+
+        //Update the new animation
+        if (enemyState == EnemyState.Idle)
+            anim.SetBool("isIdle", true);
+        else if (enemyState == EnemyState.Chasing)
+            anim.SetBool("isChasing", true);
+    }
+
+}
+
+public enum EnemyState{
+    Idle,
+    Chasing
 }
